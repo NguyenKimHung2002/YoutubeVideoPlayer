2a6fcca660d6366269d3a5d60cafe895b4d491cc
Hung <kimhungdtht@gmail.com>
2023-01-03 12:02:42 +0700

    Add project files.

---
+++ YoutubeVideoPlayer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace YoutubeVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVideoLink = new System.Windows.Forms.Label();
+            this.txtVideoLink = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtSongName = new System.Windows.Forms.TextBox();
+            this.txtSongArtist = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblVideoLink
+            // 
+            this.lblVideoLink.AutoSize = true;
+            this.lblVideoLink.Location = new System.Drawing.Point(18, 47);
+            this.lblVideoLink.Name = "lblVideoLink";
+            this.lblVideoLink.Size = new System.Drawing.Size(56, 13);
+            this.lblVideoLink.TabIndex = 0;
+            this.lblVideoLink.Text = "Video link:";
+            // 
+            // txtVideoLink
+            // 
+            this.txtVideoLink.Location = new System.Drawing.Point(80, 45);
+            this.txtVideoLink.Name = "txtVideoLink";
+            this.txtVideoLink.Size = new System.Drawing.Size(532, 20);
+            this.txtVideoLink.TabIndex = 1;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(626, 43);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(55, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(21, 112);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(660, 302);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(410, 81);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtSongName
+            // 
+            this.txtSongName.Location = new System.Drawing.Point(21, 83);
+            this.txtSongName.Name = "txtSongName";
+            this.txtSongName.Size = new System.Drawing.Size(277, 20);
+            this.txtSongName.TabIndex = 5;
+            // 
+            // txtSongArtist
+            // 
+            this.txtSongArtist.Location = new System.Drawing.Point(304, 83);
+            this.txtSongArtist.Name = "txtSongArtist";
+            this.txtSongArtist.Size = new System.Drawing.Size(100, 20);
+            this.txtSongArtist.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.txtSongArtist);
+            this.Controls.Add(this.txtSongName);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtVideoLink);
+            this.Controls.Add(this.lblVideoLink);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVideoLink;
+        private System.Windows.Forms.TextBox txtVideoLink;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtSongName;
+        private System.Windows.Forms.TextBox txtSongArtist;
+    }
+}
+
---
+++ YoutubeVideoPlayer/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExtractor;
+
+namespace YoutubeVideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            string html = "<html><head>";
+            html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'/>";
+            html += "<iframe id='video' src='https://www.youtube.com/embed/{0}' width='600' height='300' frameborder='0' allowfullscreen></iframe>";
+            html += "</head></html>";
+            this.webBrowser1.DocumentText = string.Format(html, txtVideoLink.Text.Split('=')[1]);
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var excutingPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                var youtubeDL = Path.Combine(Path.GetDirectoryName(excutingPath), @"Tool\youtube-dl.exe");
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = youtubeDL;
+                startInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+                startInfo.UseShellExecute = false;
+                startInfo.CreateNoWindow = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                //var songName = "chung ta khong thuoc ve nhau";
+                //var artists = "Son tung";
+                var command = "-x --no-continue " + "\"" + "ytsearch1: " + txtSongName.Text + " " + txtSongArtist.Text + "\"" + "--audio-format mp3 --audio-quality 0 -o " + "/Downloads/" + "\"" + txtSongName.Text + " - " + txtSongArtist.Text + "\"" + "." + "%(ext)s";
+                using (var _process = new Process())
+                {
+                    startInfo.Arguments = command;
+                    _process.StartInfo = startInfo;
+                    
+                    _process.Start();
+                    _process.WaitForExit();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
